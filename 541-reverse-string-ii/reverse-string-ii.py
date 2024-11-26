fbf732fa7949d458735c12806a57e7014a816458
haseeb-uu72 <haseeb.uu72@gmail.com>
2024-11-26 21:09:39 +0500

    Time: 3 ms (36.93%) | Memory: 17.5 MB (6.10%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,18 +1,12 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        def reverse1(s, k):
-            if len(s)<k:
-                return s[::-1]
-            if len(s)>=k and len(s)<=2*k:
-                s1 = s[:k]; s2 = s[k:]
-                s1 = list(s1)
-                i = 0; j = len(s1)-1
-                while(i<j):
-                    temp = s1[i]; s1[i] = s1[j]; s1[j] = temp
-                    i+=1; j-=1
-                s1 = ''.join(s1)
-                return s1+s2
-            else:
-                return reverse1(s[:(2*k)], k) + reverse1(s[(2*k):], k)
-        s = reverse1(s, k)
-        return s
+        # Convert the string to a list to easily modify it
+        s = list(s)
+        
+        # Loop through the string in steps of 2k
+        for i in range(0, len(s), 2 * k):
+            # Reverse the first k characters of the current 2k block
+            s[i:i + k] = s[i:i + k][::-1]
+        
+        # Convert list back to string and return it
+        return ''.join(s)
